973cc3310b6a8b628a3862808deedb453430e998
Vitor França <franc.vitoor@gmail.com>
2022-01-12 14:56:33 -0300

    Add listItem

---
+++ src/components/ListItem/index.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+
+
+interface Props {
+  children?: React.ReactNode;
+  index: number;
+  value: number;
+}
+
+function ListItem ({ children, value, index, ...other }: Props) {
+    console.log('Children: ' + children)
+    console.log('Value: ' + value)
+    console.log('Index: ' + index)
+  
+    return (
+      <div
+        role="tabpanel"
+        hidden={value !== index}
+        id={`simple-tabpanel-${index}`}
+        aria-labelledby={`simple-tab-${index}`}
+        {...other}
+      >
+        {value === index && (
+          <Box sx={{ p: 3 }}>
+            <Typography>{children}</Typography>
+          </Box>
+        )}
+      </div>
+    );
+};
+
+export default ListItem;
