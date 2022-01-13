fc73adcd1fdfd15ec7dac9f8391450b2bba8014d
Vitor França <franc.vitoor@gmail.com>
2022-01-12 21:08:05 -0300

    map list

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,27 @@
 import * as React from 'react';
+import Tabs from './components/Tabs';
 import Box from '@mui/material/Box';
+import TasksProvider from './context/Tasks';
+import { useTasks } from './context/Tasks';
 
+type ListItem = {
+  id: number,
+  task: string,
+  isDone: boolean
+};
+
+type List = ListItem[];
 
 function App() {
+  const { tasks } = useTasks();
+  console.log(tasks)
 
   return (
-    <Box sx={{ width: '100%' }}>
-    </Box>
+    <TasksProvider>
+      <Box sx={{ width: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+        <Tabs list={tasks} />
+      </Box>
+    </TasksProvider>
   );
 }
 
