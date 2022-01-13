07b2a1b519e7423d953aaacab5fc85ec66d84f40
Vitor França <franc.vitoor@gmail.com>
2022-01-12 21:07:37 -0300

    style

--- src/components/ListItem/index.tsx
+++ src/components/ListItem/index.tsx
@@ -1,32 +1,55 @@
-import React from 'react';
+import React from "react";
 
-import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
-
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import { Checkbox } from "@mui/material";
 
 interface Props {
   children?: React.ReactNode;
   index: number;
   value: number;
+  isDone: boolean;
 }
 
-function ListItem ({ children, value, index, ...other }: Props) {
-  
-    return (
-      <div
-        role="tabpanel"
-        hidden={value !== index}
-        id={`simple-tabpanel-${index}`}
-        aria-labelledby={`simple-tab-${index}`}
-        {...other}
-      >
-        {value === index && (
-          <Box sx={{ p: 3 }}>
-            <Typography>{children}</Typography>
-          </Box>
-        )}
-      </div>
-    );
-};
+function ListItem({ children, value, isDone, index, ...other }: Props) {
+  const [checked, setChecked] = React.useState(isDone);
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setChecked(event.target.checked);
+  };
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box
+          sx={{
+            p: 3,
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "center",
+          }}
+        >
+          <Checkbox
+            checked={checked}
+            onChange={handleChange}
+            inputProps={{ "aria-label": "controlled" }}
+          />
+
+          <Typography
+            sx={{ textDecoration: `${isDone ? "line-through" : "none"}` }}
+          >
+            {children}
+          </Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
 
 export default ListItem;
--- src/components/Tabs/index.tsx
+++ src/components/Tabs/index.tsx
@@ -1,59 +1,75 @@
-import React from 'react';
-import TabItem from '../ListItem';
+import React from "react";
+import TabItem from "../ListItem";
+import Header from "../Header";
 
-import Box from '@mui/material/Box';
-import TabsHeader from '@mui/material/Tabs'
-import Tab from '@mui/material/Tab';
+import TabsHeader from "@mui/material/Tabs";
+import { Box, Tab } from "@mui/material";
 
 
 type ListItem = {
-    id: number,
-    task: string,
-    isDone: boolean
+  id: number;
+  task: string;
+  isDone: boolean;
 };
 
 type Props = {
-    list: ListItem[];
+  list: ListItem[];
 };
 
-function Tabs ({ list }: Props){
-    const [value, setValue] = React.useState(0);
+function Tabs({ list }: Props) {
 
+  const [value, setValue] = React.useState(0);
 
-    const handleChange = (event: React.SyntheticEvent, newValue: number) => {
-        setValue(newValue);
-      };
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    setValue(newValue);
+  };
 
-    function a11yProps(index: number) {
-        return {
-          id: `simple-tab-${index}`,
-          'aria-controls': `simple-tabpanel-${index}`,
-        };
-      };
+  function a11yProps(index: number) {
+    return {
+      id: `simple-tab-${index}`,
+      "aria-controls": `simple-tabpanel-${index}`,
+    };
+  }
 
-    return <Box sx={{ width: '100%' }}>
-                <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-                    <TabsHeader value={value} onChange={handleChange} aria-label="basic tabs example">
-                        <Tab label="Item One" {...a11yProps(0)} />
-                        <Tab label="Item Two" {...a11yProps(1)} />
-                        <Tab label="Item Three" {...a11yProps(2)} />
-                    </TabsHeader>
-                </Box>
-                <Box>
-                    {
-                        list.map(({id, task, isDone}, i) => {
-                            let index: number = 0;
-                            if(value) index = isDone ? 2 : 1;
+  const TabWidth = `${100 / 3}%`;
 
-                            return (
-                                <TabItem key={id} value={value} index={index}>
-                                    {task}
-                                </TabItem>
-                            )}
-                        )
-                    }
-                </Box>
-        </Box>;
-};
+  return (
+    <Box
+      sx={{
+        width: "40%",
+        border: 1,
+        borderRadius: 3,
+        padding: 2,
+        borderColor: "divider",
+      }}
+    >
+      <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+        <Header />
+        <TabsHeader
+          sx={{ width: "100%" }}
+          value={value}
+          onChange={handleChange}
+          aria-label="basic tabs example"
+        >
+          <Tab sx={{ width: TabWidth }} label="Todos" {...a11yProps(0)} />
+          <Tab sx={{ width: TabWidth }} label="Completo" {...a11yProps(1)} />
+          <Tab sx={{ width: TabWidth }} label="Incompleto" {...a11yProps(2)} />
+        </TabsHeader>
+      </Box>
+      <Box>
+        {list && list.map(({ id, task, isDone }, i) => {
+          let index: number = 0;
+          if (value) index = isDone ? 1 : 2;
+
+          return (
+            <TabItem key={id} isDone={isDone} value={value} index={index}>
+              {task}
+            </TabItem>
+          );
+        })}
+      </Box>
+    </Box>
+  );
+}
 
 export default Tabs;
